12dbfe262b69be325eee8d8d20d7f22b55769dba
Al <albarrentine@gmail.com>
2024-01-08 19:01:43 -0500

    [hirschberg] adding a generic version of Hirschberg's divide-and-conquer algorithm for obtaining a full sequence alignment, a sequence of edit operations, a longest common subsequence, etc. in linear space without needing to build/retain the full cost matrix as would be true with the backtracking approach. The space is linear in the length of the shorter string and uses divide-and-conquer. Myers and Miller observe that this formulation can apply to any cost-only linear space dynamic programming algorithm on sequences. This version takes an arbitrary cost function which accepts two strings and their lengths along with a flag for whether to reverse the string, and the costs as a single array of a specified type (usually two rows of the smaller side of the cost matrix, or three if considering transposes). The cost function will only be called log(m) times where m is the size of the larger string. This function returns an array of the simplest subproblems, the leaves of a depth-first search of the recursion. All pointers are to the original string so there are no copies and any intermediate space used (the stack in depth-first-search traversal) is log(m).

---
+++ src/hirschberg.h
@@ -0,0 +1,101 @@
+#ifndef HIRSCHBERG_H
+#define HIRSCHBERG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+#include "vector/vector.h"
+
+typedef struct {
+    const char *s1;
+    size_t m;
+    const char *s2;
+    size_t n;
+} string_subproblem_t;
+
+VECTOR_INIT(string_subproblem_array, string_subproblem_t);
+
+
+#define HIRSCHBERG(name, cost_type, cost_function)                                          \
+    bool name##_subproblems(const char *s1, size_t m, const char *s2, size_t n,             \
+                            cost_type *costs, cost_type *rev_costs, size_t cost_size,       \
+                            string_subproblem_array *stack,                                 \
+                            string_subproblem_array *result) {                              \
+        if (m == 0 || n == 0) return false;                                                 \
+        if (m < n) {                                                                        \
+            const char *tmp = s1;                                                           \
+            s1 = s2;                                                                        \
+            s2 = tmp;                                                                       \
+            size_t tmp_n = m;                                                               \
+            m = n;                                                                          \
+            n = tmp_n;                                                                      \
+        }                                                                                   \
+                                                                                            \
+        size_t cost = 0;                                                                    \
+        if (stack->n > 0) {                                                                 \
+            string_subproblem_array_clear(stack);                                           \
+        }                                                                                   \
+        if (result->n > 0) {                                                                \
+            string_subproblem_array_clear(result);                                          \
+        }                                                                                   \
+                                                                                            \
+        string_subproblem_t prob = (string_subproblem_t) {                                  \
+            .s1 = s1,                                                                       \
+            .m = m,                                                                         \
+            .s2 = s2,                                                                       \
+            .n = n                                                                          \
+        };                                                                                  \
+        string_subproblem_array_push(stack, prob);                                          \
+                                                                                            \
+        string_subproblem_t sub;                                                            \
+        while (string_subproblem_array_pop(stack, &sub)) {                                  \
+            if (sub.m == 0 || sub.n == 0 || (sub.m == 1 && sub.n == 1)) {                   \
+                string_subproblem_array_push(result, sub);                                  \
+                continue;                                                                   \
+            }                                                                               \
+            size_t sub_m = floor((double)sub.m / 2.0);                                      \
+                                                                                            \
+            memset(costs, 0, sizeof(cost_type) * cost_size);                                \
+            bool rev = false;                                                               \
+            cost_function(sub.s1, sub_m, sub.s2, sub.n, rev, costs);                        \
+            cost_type lc = costs[sub.n];                                                    \
+            memset(rev_costs, 0, sizeof(cost_type) * cost_size);                            \
+            rev = true;                                                                     \
+            cost_function(sub.s1 + sub_m, sub.m - sub_m, sub.s2, sub.n, rev, rev_costs);    \
+            cost_type rc = rev_costs[sub.n];                                                \
+            cost_type max_sum = (cost_type) 0;                                              \
+            size_t sub_n = 0;                                                               \
+            for (size_t j = 0; j < sub.n + 1; j++) {                                        \
+                costs[j] += rev_costs[sub.n - j];                                           \
+                if (costs[j] > max_sum || (sub_n == 0 && j > 0 && costs[j] == max_sum)) {   \
+                    sub_n = j;                                                              \
+                    max_sum = costs[j];                                                     \
+                }                                                                           \
+            }                                                                               \
+                                                                                            \
+            string_subproblem_t left_sub = (string_subproblem_t) {                          \
+                .s1 = sub.s1,                                                               \
+                .m = sub_m,                                                                 \
+                .s2 = sub.s2,                                                               \
+                .n = sub_n                                                                  \
+            };                                                                              \
+            string_subproblem_t right_sub = (string_subproblem_t) {                         \
+                .s1 = sub.s1 + sub_m,                                                       \
+                .m = sub.m - sub_m,                                                         \
+                .s2 = sub.s2 + sub_n,                                                       \
+                .n = sub.n - sub_n                                                          \
+            };                                                                              \
+                                                                                            \
+            string_subproblem_array_push(stack, right_sub);                                 \
+            string_subproblem_array_push(stack, left_sub);                                  \
+                                                                                            \
+        }                                                                                   \
+                                                                                            \
+        return true;                                                                        \
+    }
+
+
+#endif
