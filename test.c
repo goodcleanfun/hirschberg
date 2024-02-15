0e646aaafda71359011b6ed021bd5624adc42743
Al <albarrentine@gmail.com>
2024-02-15 04:16:48 -0500

    [test] updating tests to use new API

--- test.c
+++ test.c
@@ -76,13 +76,10 @@ lcs_test_t test_data_lcs[] = {
     }
 };
 
-void test_hirschberg_lcs_cost(const char *s1, size_t m, const char *s2, size_t n, bool reverse, uint64_t *costs) {
-    uint64_t diag = 0;
-    uint64_t up = 0;
-    uint64_t left = 0;
+void test_hirschberg_lcs_cost(const char *s1, size_t m, const char *s2, size_t n, bool reverse, uint64_t *costs, size_t costs_size) {
     uint64_t best_cost = m + n;
     uint64_t *cur_lcs = costs;
-    uint64_t *prev_lcs = costs + n + 1;
+    uint64_t *prev_lcs = costs + (costs_size / 2);
     for (size_t i = 1; i < m + 1; i++) {
         char c1 = !reverse ? tolower(s1[i - 1]) : tolower(s1[m - i]);
         for (size_t j = 1; j < n + 1; j++) {
@@ -106,50 +103,42 @@ void test_hirschberg_lcs_cost(const char *s1, size_t m, const char *s2, size_t n
 }
 
 
-void test_hirschberg_lcs_utf8_cost(const char *s1, size_t m, const char *s2, size_t n, bool reverse, uint64_t *costs) {
-    uint64_t diag = 0;
-    uint64_t up = 0;
-    uint64_t left = 0;
+void test_hirschberg_lcs_utf8_cost(const char *s1, size_t m, const char *s2, size_t n, bool reverse, uint64_t *costs, size_t costs_size) {
     uint64_t best_cost = m + n;
     uint64_t *cur_lcs = costs;
-    uint64_t *prev_lcs = costs + n + 1;
+    uint64_t *prev_lcs = costs + (costs_size / 2);
     const unsigned char *s1_ptr = (const unsigned char *) s1;
     const unsigned char *s2_ptr;
-    size_t s1_len, s2_len;
     size_t s1_consumed = 0;
     size_t s2_consumed = 0;
-    if (reverse) {
-        s1_len = strlen(s1);
-        s2_len = strlen(s2);
-    }
     int32_t c1;
     int32_t c2;
     int32_t prev_c1;
     int32_t prev_c2;
-    for (size_t i = 1; i < m + 1; i++) {
+    size_t i = 1;
+    while (s1_consumed < m) {
         c1 = 0;
-        utf8proc_ssize_t c1_len;
+        ssize_t c1_len;
         if (!reverse) {
             c1_len = utf8proc_iterate(s1_ptr, -1, &c1);
         } else {
-            c1_len = utf8proc_iterate_reversed(s1_ptr, s1_len - s1_consumed, &c1);
+            c1_len = utf8proc_iterate_reversed(s1_ptr, m - s1_consumed, &c1);
         }
         if (c1 == 0 || c1_len <= 0) break;
         c1 = utf8proc_tolower(c1);
         s2_ptr = (const unsigned char *) s2;
         s2_consumed = 0;
-        for (size_t j = 1; j < n + 1; j++) {
+        size_t j = 1;
+        while (s2_consumed < n) {
             c2 = 0;
-            utf8proc_ssize_t c2_len;
+            ssize_t c2_len;
             if (!reverse) {
                 c2_len = utf8proc_iterate(s2_ptr, -1, &c2);
             } else {
-                c2_len = utf8proc_iterate_reversed(s2_ptr, s2_len - s2_consumed, &c2);
+                c2_len = utf8proc_iterate_reversed(s2_ptr, n - s2_consumed, &c2);
             }
             if (c2 == 0 || c2_len == 0) break;
-
             c2 = utf8proc_tolower(c2);
-
             uint64_t val = 0;
             if (c1 == c2) {
                 val = prev_lcs[j - 1] + 1;
@@ -163,66 +152,73 @@ void test_hirschberg_lcs_utf8_cost(const char *s1, size_t m, const char *s2, siz
             cur_lcs[j] = val;
             if (!reverse) {
                 s2_ptr += c2_len;
-            } else {
-                s2_consumed += c2_len;
             }
+            s2_consumed += c2_len;
             prev_c2 = c2;
+            j++;
         }
         for (size_t j = 0; j < n + 1; j++) {
             prev_lcs[j] = cur_lcs[j];
         }
         if (!reverse) {
             s1_ptr += c1_len;
-        } else {
-            s1_consumed += c1_len;
         }
-
+        s1_consumed += c1_len;
         prev_c1 = c1;
+        i++;
     }
 }
 
-char *hirschberg_alignment_lcs(string_subproblem_array *result, size_t max_len) {
-    char *alignment = malloc(max_len + 1);
+char *hirschberg_alignment_lcs(hirschberg_uint64_sim_iter *iter, size_t max_len) {
+    char *alignment = malloc(sizeof(char) * (max_len + 1));
     size_t idx = 0;
-
-    utf8proc_ssize_t c1_len, c2_len;
+    ssize_t c1_len, c2_len;
     int32_t c1, c2;
     const unsigned char *s1_ptr;
     const unsigned char *s2_ptr;
 
-    for (size_t i = 0; i < result->n; i++) {
-        string_subproblem_t sub = result->a[i];
-        if (sub.n == 1) {
-            c2_len = utf8proc_iterate((const unsigned char *) sub.s2, -1, &c2);
-            const unsigned char *s1_ptr = (const unsigned char *) sub.s1;
-            for (size_t j = 0; j < sub.m; j++) {
-                c1_len = utf8proc_iterate(s1_ptr, -1, &c1);
-                if (utf8proc_tolower(c2) == utf8proc_tolower(c1)) {
-                    for (size_t k = 0; k < c2_len; k++) {
-                        alignment[idx++] = *(sub.s2 + k);
+    const char *s1 = iter->input.s1;
+    const char *s2 = iter->input.s2;
+
+    bool result = true;
+    size_t i = 0;
+    while (hirschberg_uint64_sim_iter_next(iter)) {
+        if (iter->is_result) {
+            string_subproblem_t sub = iter->sub;
+            size_t um = utf8_len(s1 + sub.x, sub.m);
+            size_t un = utf8_len(s2 + sub.y, sub.n);
+            if (un == 1) {
+                c2_len = utf8proc_iterate((const unsigned char *) s2 + sub.y, -1, &c2);
+                const unsigned char *s1_ptr = (const unsigned char *) s1 + sub.x;
+                for (size_t j = 0; j < um; j++) {
+                    c1_len = utf8proc_iterate(s1_ptr, -1, &c1);
+                    if (utf8proc_tolower(c2) == utf8proc_tolower(c1)) {
+                        for (size_t k = 0; k < c2_len; k++) {
+                            alignment[idx++] = *(s2 + sub.y + k);
+                        }
+                        break;
                     }
-                    break;
                 }
-                s1_ptr += c1_len;
-            }
-        } else if (sub.n == 2 && sub.m == 2) {
-            alignment[idx++] = '/';
-            alignment[idx++] = '\\';
-        } else if (sub.m == 1) {
-            c2_len = utf8proc_iterate((const unsigned char *) sub.s1, -1, &c2);
-            const unsigned char *s2_ptr = (const unsigned char *) sub.s2;
-            for (size_t j = 0; j < sub.n; j++) {
-                c1_len = utf8proc_iterate(s2_ptr, -1, &c1);
-                if (c2 == c1) {
-                    for (size_t k = 0; k < c2_len; k++) {
-                        alignment[idx++] = *(sub.s1 + k);
+            } else if (um == 2 && un == 2) {
+                alignment[idx++] = '/';
+                alignment[idx++] = '\\';
+            } else if (um == 1) {
+                c2_len = utf8proc_iterate((const unsigned char *) s1 + sub.x, -1, &c2);
+                const unsigned char *s2_ptr = (const unsigned char *) s2 + sub.y;
+                for (size_t j = 0; j < un; j++) {
+                    c1_len = utf8proc_iterate(s2_ptr, -1, &c1);
+                    if (c2 == c1) {
+                        for (size_t k = 0; k < c2_len; k++) {
+                            alignment[idx++] = *(s1 + sub.x + k);
+                        }
+                        break;
                     }
-                    break;
                 }
-                s2_ptr += c1_len;
             }
+            i++;
         }
     }
+
     alignment[idx] = '\0';
     return alignment;
 }
@@ -233,8 +229,9 @@ bool test_hirschberg_subproblem_lcs(lcs_test_t test) {
     const char *s2 = test.s2;
     size_t m = strlen(s1);
     size_t n = strlen(s2);
-    size_t max_len = m + n;
+    size_t max_len = m;
     if (n > m) {
+        max_len = n;
         const char *tmp = s1;
         s1 = s2;
         s2 = tmp;
@@ -248,43 +245,29 @@ bool test_hirschberg_subproblem_lcs(lcs_test_t test) {
     size_t un = utf8_len(s2, n);
     if (um != m || un != n) {
         is_utf8 = true;
-        m = um;
-        n = un;
     }
 
-    string_subproblem_array *stack = string_subproblem_array_new();
-    string_subproblem_array *result = string_subproblem_array_new();
+    size_t values_size = (un + 1) * 2;
 
-    size_t costs_size = (n + 1) * 2;
+    hirschberg_uint64_sim_iter *iter = hirschberg_uint64_sim_iter_new(
+        (string_pair_input_t){.s1 = s1, .m = m, .s2 = s2, .n = n},
+        (hirschberg_options_t){.utf8 = is_utf8, .allow_transpose = true, .zero_out_memory = true},
+        hirschberg_uint64_sim_values_new(values_size),
+        hirschberg_uint64_sim_function_new(is_utf8 ? test_hirschberg_lcs_utf8_cost : test_hirschberg_lcs_cost)
+    );
 
-    uint64_t *costs = malloc(sizeof(size_t) * costs_size * 2);
-    uint64_t *rev_costs = costs + costs_size;
+    char *alignment = hirschberg_alignment_lcs(iter, max_len);
 
-    bool allow_transpose = true;
-    hirschberg_context_t context = {
-        .utf8 = is_utf8,
-        .allow_transpose = allow_transpose,
-        .stack = stack,
-        .result = result,
-    };
-
-    bool success = hirschberg_uint64_sim_subproblems(s1, m, s2, n, context,
-                                                    !is_utf8 ? test_hirschberg_lcs_cost : test_hirschberg_lcs_utf8_cost,
-                                                    costs, rev_costs, costs_size);
-
-    char *alignment = hirschberg_alignment_lcs(result, max_len);
-    if (strncmp(alignment, test.expected_lcs, strlen(test.expected_lcs)) != 0) {
+    bool success = strncmp(alignment, test.expected_lcs, strlen(test.expected_lcs)) == 0;
+    if (!success) {
         printf("s1: %s\n", s1);
         printf("s2: %s\n", s2);
         printf("alignment: %s\n", alignment);
         printf("expected: %s\n", test.expected_lcs);
-        success = false;
     }
 
+    hirschberg_uint64_sim_iter_destroy(iter);
     free(alignment);
-    free(costs);
-    string_subproblem_array_destroy(stack);
-    string_subproblem_array_destroy(result);
 
     return success;
 }
